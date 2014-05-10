c1df03c89abd31319f9ca6d6e312ae2965c0588e
jimmymac205 <jimmymac_205@yahoo.com>
2014-05-10 15:38:30 -0500

    Final 1
    Fixes winning statement, mobile picture fix still needed

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -126,7 +126,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win Happy Mother's Day!!!" : "Game over! (We still love you anyway)";
+  var message = won ? "You win! Happy Mother's Day!!!" : "Game over! (We still love you)";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
